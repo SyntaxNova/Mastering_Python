bf756003a9ab8407726c622fea4a24ca494b60c4
“Atharva <atharvacodes@gmail.com>
2024-06-04 16:42:36 +0530

    D08

---
+++ pyfile.py
@@ -0,0 +1,9 @@
+with open("practice.txt" , "w") as f:
+    f.write("hello how are you ,\ni am fine thank you ")
+
+with open("practice.txt" , "r") as f:
+    data = f.read()
+newdata = data.replace("you" , "me")
+
+with open("practice.txt" , "w") as f:
+    f.write(newdata)
